b50dab3258a98175830a92613083acb8f893756c
Vaibhaw Jaiswal <vaibhaw@Vaibhaws-MacBook-Air.local>
2017-09-07 02:38:32 +0530

    Circular json error

--- server/src/api/controllers/rsvpController.js
+++ server/src/api/controllers/rsvpController.js
@@ -2,14 +2,15 @@ import axios from 'axios';
 import CircularJSON from 'circular-json';
 
 const getRsvp = async (phone) => {
-    let oldRsvp = await axios.get(`http://localhost:5984/rsvp/${phone}`)
+    let oldRsvpPromise = await axios.get(`http://localhost:5984/rsvp/${phone}`)
         .then((response) => {
             return CircularJSON.stringify(response).data;
         })
         .catch((error) => {
             return null;
         });
-   
+
+    let oldRsvp = await Promise.resolve(oldRsvpPromise);
     return oldRsvp;
 }
 
@@ -17,40 +18,40 @@ export const submitRsvp = async (req, res) => {
     let rsvp = req.body;
     let oldRsvp = await getRsvp(rsvp.phone);
 
-    if(oldRsvp === null){
+    if (oldRsvp === null) {
         await axios.post("http://localhost:5984/rsvp", {
-        "name": rsvp.name,
-        "phone": rsvp.phone,
-        "email": rsvp.email,
-        "isComing": rsvp.isComing,
-        "msg": rsvp.msg,
-        "_id": rsvp.phone,
-        "lastSaved": new Date().toString()
-    }).then((response) => {
-            res.json(CircularJSON.stringify(response).data);
+            "name": rsvp.name,
+            "phone": rsvp.phone,
+            "email": rsvp.email,
+            "isComing": rsvp.isComing,
+            "msg": rsvp.msg,
+            "_id": rsvp.phone,
+            "lastSaved": new Date().toString()
+        }).then((response) => {
+            res.status(200).send('Saved successfully!');
         })
-        .catch((error) => {
-            res.send(CircularJSON.stringify(error));
-        })
-       
+            .catch((error) => {
+                res.status(500).send(CircularJSON.stringify(error));
+            })
+
     } else {
         await axios.put(`http://localhost:5984/rsvp/${rsvp.phone}`, {
-        "name": rsvp.name,
-        "phone": rsvp.phone,
-        "email": rsvp.email,
-        "isComing": rsvp.isComing,
-        "msg": rsvp.msg,
-        "_id": rsvp.phone,
-        "_rev": oldRsvp._rev,
-        "lastSaved": new Date().toString()
-    })
-        .then((response) => {
-            res.json(CircularJSON.stringify(response).data);
+            "name": rsvp.name,
+            "phone": rsvp.phone,
+            "email": rsvp.email,
+            "isComing": rsvp.isComing,
+            "msg": rsvp.msg,
+            "_id": rsvp.phone,
+            "_rev": oldRsvp._rev,
+            "lastSaved": new Date().toString()
         })
-        .catch((error) => {
-            res.send(CircularJSON.stringify(error));
-        })
-    }    
+            .then((response) => {
+                res.status(200).send('Updated Successfully!');
+            })
+            .catch((error) => {
+                res.status(500).send(CircularJSON.stringify(error));
+            })
+    }
     console.log('done!!!');
 }
 
